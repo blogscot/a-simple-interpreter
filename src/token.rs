ffe7044d3889d35b7e8f282c4323aeb2b6fe79bb
blogscot <iain.diamond@gmail.com>
2018-04-22 20:26:15 +0100

    Restructure code

---
+++ src/token.rs
@@ -0,0 +1,32 @@
+use std::fmt;
+
+#[derive(Clone, Debug, PartialEq)]
+pub enum TokenType {
+  Integer(i32),
+  Plus,
+  Minus,
+  EOF,
+}
+
+impl fmt::Display for TokenType {
+  fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+    let output = match self {
+      TokenType::Integer(value) => format!("Integer, {}", value),
+      TokenType::Plus => "Plus".into(),
+      TokenType::Minus => "Minus".into(),
+      TokenType::EOF => "EOF".into(),
+    };
+    write!(f, "{}", output)
+  }
+}
+
+#[derive(Clone, Debug)]
+pub struct Token {
+  pub token_type: TokenType,
+}
+
+impl fmt::Display for Token {
+  fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+    write!(f, "Token({})", self.token_type)
+  }
+}
