24c1a54f620647a4bc03975d28aae1ed7f257630
blogscot <iain.diamond@gmail.com>
2018-04-23 12:35:08 +0100

    Introduce testing

--- src/main.rs
+++ src/main.rs
@@ -9,3 +9,25 @@ fn main() {
 
     println!("{}", result);
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn add_two_single_digit_numbers() {
+        let mut interpreter = Interpreter::new("4 + 7".into());
+        let result = interpreter.expr();
+
+        assert_eq!(result, 11);
+    }
+
+    #[test]
+    fn subtract_two_single_digit_numbers() {
+        let mut interpreter = Interpreter::new("4 - 7".into());
+        let result = interpreter.expr();
+
+        assert_eq!(result, -3);
+    }
+
+}
