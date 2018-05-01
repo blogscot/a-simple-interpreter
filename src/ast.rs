5d91bf6c96722e4b8e0c13057ce9c158b8ddf53d
Iain Diamond <iain.diamond@gmail.com>
2018-05-01 18:55:27 +0100

    Update test cases

--- src/ast.rs
+++ src/ast.rs
@@ -21,3 +21,88 @@ impl Node for Ast {
     self.root_node.accept(visitor)
   }
 }
+
+#[cfg(test)]
+mod tests {
+  use super::*;
+
+  #[test]
+  fn add_two_single_digit_numbers() {
+    let ast = Ast::new("4 + 7");
+    let result = ast.evaluate();
+
+    assert_eq!(result, 11);
+  }
+
+  #[test]
+  fn subtract_two_single_digit_numbers() {
+    let ast = Ast::new("4 - 7");
+    let result = ast.evaluate();
+
+    assert_eq!(result, -3);
+  }
+
+  #[test]
+  fn multiply_two_single_digit_numbers() {
+    let ast = Ast::new("4 * 7");
+    let result = ast.evaluate();
+
+    assert_eq!(result, 28);
+  }
+
+  #[test]
+  fn divide_two_single_digit_numbers() {
+    let ast = Ast::new("10 / 3");
+    let result = ast.evaluate();
+
+    assert_eq!(result, 3);
+  }
+
+  #[test]
+  fn add_multiple_digit_numbers() {
+    let ast = Ast::new("101 + 99");
+    let result = ast.evaluate();
+
+    assert_eq!(result, 200);
+  }
+
+  #[test]
+  fn subtract_multiple_digit_numbers() {
+    let ast = Ast::new("1234 - 134");
+    let result = ast.evaluate();
+
+    assert_eq!(result, 1100);
+  }
+
+  #[test]
+  fn add_multiple_numbers() {
+    let ast = Ast::new("1 + 2 + 3 + 4 + 5");
+    let result = ast.evaluate();
+
+    assert_eq!(result, 15);
+  }
+
+  #[test]
+  fn add_and_subtract_multiple_numbers() {
+    let ast = Ast::new("1 + 2 - 3 + 4 - 5");
+    let result = ast.evaluate();
+
+    assert_eq!(result, -1);
+  }
+
+  #[test]
+  fn muliply_and_divide_multiple_numbers() {
+    let ast = Ast::new("10 * 20 / 2 / 10");
+    let result = ast.evaluate();
+
+    assert_eq!(result, 10);
+  }
+
+  #[test]
+  fn evaluate_multiterm_expression_contain_parens() {
+    let ast = Ast::new("6 * (3 + 7) / 2");
+    let result = ast.evaluate();
+
+    assert_eq!(result, 30);
+  }
+}
--- src/parser.rs
+++ src/parser.rs
@@ -64,7 +64,7 @@ impl Parser {
 
       if token_type == Multiply || token_type == Divide {
         self.consume(&token_type);
-        node = Box::new(BinOpNode::new(self.factor(), node, token_type));
+        node = Box::new(BinOpNode::new(node, self.factor(), token_type));
       }
     }
     node
@@ -78,106 +78,12 @@ impl Parser {
 
       if token_type == Plus || token_type == Minus {
         self.consume(&token_type);
-        node = Box::new(BinOpNode::new(self.factor(), node, token_type));
+        node = Box::new(BinOpNode::new(node, self.factor(), token_type));
       }
     }
     node
   }
   pub fn parse(&mut self) -> Box<Node> {
     self.expr()
-  }
-}
-
-#[cfg(test)]
-mod tests {
-  use super::*;
-
-  #[test]
-  fn add_two_single_digit_numbers() {
-    let mut interpreter = Interpreter::new("4 + 7".into());
-    let result = interpreter.expr();
-
-    // assert_eq!(result, 11);
-  }
-
-  #[test]
-  #[ignore]
-  fn subtract_two_single_digit_numbers() {
-    let mut interpreter = Interpreter::new("4 - 7".into());
-    let result = interpreter.expr();
-
-    // assert_eq!(result, -3);
-  }
-
-  #[test]
-  #[ignore]
-  fn multiply_two_single_digit_numbers() {
-    let mut interpreter = Interpreter::new("4 * 7".into());
-    let result = interpreter.expr();
-
-    // assert_eq!(result, 28);
-  }
-
-  #[test]
-  #[ignore]
-  fn divide_two_single_digit_numbers() {
-    let mut interpreter = Interpreter::new("10 / 3".into());
-    let result = interpreter.expr();
-
-    // assert_eq!(result, 3);
-  }
-
-  #[test]
-  #[ignore]
-  fn add_multiple_digit_numbers() {
-    let mut interpreter = Interpreter::new("101 + 99".into());
-    let result = interpreter.expr();
-
-    // assert_eq!(result, 200);
-  }
-
-  #[test]
-  #[ignore]
-  fn subtract_multiple_digit_numbers() {
-    let mut interpreter = Interpreter::new("1234 - 134".into());
-    let result = interpreter.expr();
-
-    // assert_eq!(result, 1100);
-  }
-
-  #[test]
-  #[ignore]
-  fn add_multiple_numbers() {
-    let mut interpreter = Interpreter::new("1 + 2 + 3 + 4 + 5".into());
-    let result = interpreter.expr();
-
-    // assert_eq!(result, 15);
-  }
-
-  #[test]
-  #[ignore]
-  fn add_and_subtract_multiple_numbers() {
-    let mut interpreter = Interpreter::new("1 + 2 - 3 + 4 - 5".into());
-    let result = interpreter.expr();
-
-    // assert_eq!(result, -1);
-  }
-
-  #[test]
-  #[ignore]
-  fn muliply_and_divide_multiple_numbers() {
-    let mut interpreter = Interpreter::new("10 * 20 / 2 / 10".into());
-    let result = interpreter.expr();
-
-    // assert_eq!(result, 10);
-  }
-
-  #[test]
-  #[ignore]
-  fn evaluate_multiterm_expression_contain_parens() {
-    let mut interpreter = Interpreter::new("6 * (3 + 7) / 2".into());
-    let result = interpreter.expr();
-
-    // assert_eq!(result, 30);
   }
 }
