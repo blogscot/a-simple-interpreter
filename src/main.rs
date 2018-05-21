5c2421ed03197ac20939d22488c38311af229152
Iain Diamond <iain.diamond@gmail.com>
2018-05-21 16:12:32 +0100

    Read program from external file

--- src/main.rs
+++ src/main.rs
@@ -18,14 +18,37 @@ mod table_builder;
 mod token;
 mod visitor;
 
+use std::env;
+use std::error;
+use std::fs::File;
+use std::io::prelude::*;
+use std::io::BufReader;
+
 use interpreter::Interpreter;
 
-fn main() {
-  let mut interpreter = Interpreter::new(
-    r#"
-    PROGRAM empty;
-    BEGIN
-    END."#,
-  );
-  println!("{:?}", interpreter.interpret());
+fn read_from_file(filename: &str) -> std::io::Result<String> {
+  let file = File::open(filename)?;
+  let mut buf_reader = BufReader::new(file);
+  let mut contents = String::new();
+  buf_reader.read_to_string(&mut contents)?;
+  Ok(contents)
+}
+
+///
+///  Read and Interpret a Pascal file.
+///
+/// Use:
+///   ./target/debug/basic-interpreter <filename>
+///
+fn main() -> Result<(), Box<error::Error>> {
+  let args: Vec<String> = env::args().collect();
+  let filename = &args[1];
+  let source = read_from_file(&filename)?;
+
+  println!("Processing file: {}", filename);
+  let mut interpreter = Interpreter::new(source.as_str());
+  if interpreter.interpret() == number::Number::Nil {
+    println!("Success!");
+  }
+  Ok(())
 }
